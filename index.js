f027e20476e8755c35f57fce88b7312441edbd0a
koushikroy-440 <koushikroy295@gmail.com>
2020-08-27 20:47:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const {app,BrowserWindow} = require('electron');
+app.on('ready',build_app);
+function build_app(){
+    var app_window = new BrowserWindow({
+        webPreferences : {
+            nodeIntegration : true,
+            webviewTag:true
+        },
+        title:'Download manager'
+    });
+    app_window.setMenu(null);
+    app_window.loadFile('./assests/index.html');
+}
