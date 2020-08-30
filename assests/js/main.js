5d41e5354d6a507002853bdf11bab3c5ff349d90
koushikroy-440 <koushikroy295@gmail.com>
2020-08-30 19:24:49 +0530

    made changes

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,24 +1,27 @@
-// window.onload = function(){
-//     responsive();
-//     open_url();
-// }
-// window.onresize = function(){
-//     responsive();
-// }
-// //webpage responsive
-// function responsive(){
-//     var header_height = document.querySelector("header").offsetHeight;
-//     var app_height = document.querySelector("html").clientHeight;
-//     var rest_height = app_height - header_height;
-//     document.querySelector("webview").style.height = rest_height+"px";
-// }
+window.onload = function(){
+    responsive();
+    open_url();
+}
+window.onresize = function(){
+    responsive();
+}
+//webpage responsive
+function responsive(){
+    var header_height = document.querySelector("header").offsetHeight;
+    var app_height = document.querySelector("html").clientHeight;
+    var rest_height = app_height - header_height;
+    document.querySelector("webview").style.height = rest_height+"px";
+}
 
-// //open url
-// function open_url(){
-//     $("form").submit(function(){
-//         var url = $(".search").val();
-//         var webview = $("webview");
-//         webview.attr("src",url);
-//         return false;
-//     });
-// }
+//open url
+function open_url(){
+    
+    $("form").submit(function(event){
+        event.preventDefault();
+        var user_input = $(".search").val();
+        var webview = $("webview");
+        webview.attr("src",user_input);
+        
+    
+    });
+}
